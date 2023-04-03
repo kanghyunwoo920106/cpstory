8777dc3e2b31fb543c70ac5d79e0fe972e2d08fd
hyunwoo <65879478+kanghyunwoo0106@users.noreply.github.com>
2023-04-03 16:09:03 +0900

    서버 라우팅 수정

--- server/server.js
+++ server/server.js
@@ -21,10 +21,6 @@ app.get("/", function (req, res, next) {
   res.send({ message: "ok" });
 });
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/build/index.html"));
-});
-
 app.get("/add", (req, res) => {
   res.sendFile(path.join(__dirname, "/build/index.html"));
 });
